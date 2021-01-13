a2590e65c48ccd24ed61e4b60e6784ea3c6f1166
Eric Vallee <eric_vallee@webificservices.com>
2021-01-12 22:06:11 -0500

    Added concurrency argument to generate-manifest command. Updated GetManifest method signature to match the signature of the other methods in the sdk.

--- cmd/generate-manifest.go
+++ cmd/generate-manifest.go
@@ -15,14 +15,17 @@ func generateManifestGenerationCmd() *cobra.Command {
 	var extras bool
 	var extraTypeFilters []string
 	var outputPath string
+	var concurrency int
 
 	manifestGenerationCmd := &cobra.Command{
 		Use:   "generate-manifest",
 		Short: "Generate a download manifest from the GOG Api",
 		Run: func(cmd *cobra.Command, args []string) {
+			sdkPtr.GetManifest(gameTitleFilter, concurrency)
 			fmt.Println("currently a noop")
 		},
 	}
+
 	manifestGenerationCmd.Flags().StringArrayVarP(&oses, "os", "o", []string{}, "If you want to include only specific oses. Valid values: windows, mac, linux")
 	manifestGenerationCmd.Flags().StringArrayVarP(&languages, "lang", "l", []string{}, "If you want to include only specific languages. Valid values: english, french, spanish, dutch, portuguese_brazilian, russian, korean, chinese_simplified, japanese, polish, italian, german, czech, hungarian, portuguese, danish, finnish, swedish, turkish, arabic, romanian")
 	manifestGenerationCmd.Flags().StringArrayVarP(&gameTagFilters, "tag", "a", []string{}, "If you want to include only games having specific tags")
@@ -31,5 +34,6 @@ func generateManifestGenerationCmd() *cobra.Command {
 	manifestGenerationCmd.Flags().BoolVarP(&extras, "extras", "e", true, "Whether to incluse extras")
 	manifestGenerationCmd.Flags().StringArrayVarP(&extraTypeFilters, "extratype", "x", []string{}, "If you want to include only extras whole type contain one of the given strings. Look at full generated manifest without this flag to figure out valid types")
 	manifestGenerationCmd.Flags().StringVarP(&outputPath, "outputpath", "p", "", "Path representing a file to write the manifest in. If omitted, the manifest will be outputed on the terminal in json format")
+	manifestGenerationCmd.Flags().IntVarP(&concurrency, "concurrency", "r", 10, "Maximum number of concurrent requests that will be made on the GOG api")
 	return manifestGenerationCmd
 }

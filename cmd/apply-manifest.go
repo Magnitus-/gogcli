fd7416126f7ab0b9ffaa3b1426f774bedac1c5ef
Eric Vallee <eric_vallee@webificservices.com>
2021-01-28 00:28:34 -0500

    Started working on the command to apply a manifest to a storage

---
+++ cmd/apply-manifest.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"gogcli/manifest"
+	"io/ioutil"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func generateApplyManifestFsCmd(m *manifest.Manifest) *cobra.Command {
+	var path string
+
+	applyManifestFsCmd := &cobra.Command{
+		Use:   "fs",
+		Short: "Apply the manifest on a file system",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			_, err := os.Stat(path)
+			if err != nil {
+				if os.IsNotExist(err) {
+					err = os.MkdirAll(path, 0755)
+					if err != nil {
+						fmt.Println("Failed to create a directory at the specified path: ", err)
+						os.Exit(1)
+					}
+				} else {
+					fmt.Println("An error occured while trying to gather info on the specified path: ", err)
+					os.Exit(1)
+				}
+			}
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Placeholder")
+		},
+	}
+
+	applyManifestFsCmd.Flags().StringVarP(&path, "path", "p", "games", "Path to the directory where game files should be stored")
+
+	return applyManifestFsCmd
+}
+
+func generateApplyManifestCmd() *cobra.Command {
+	var m manifest.Manifest
+	var manifestPath string
+
+	applyManifestCmd := &cobra.Command{
+		Use:   "apply-manifest",
+		Short: "Change the files in a given storage to match the content of a manifest, uploading and deleting files as necessary",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			bs, err := ioutil.ReadFile(manifestPath)
+			if err != nil {
+				fmt.Println("Could not load the manifest: ", err)
+				os.Exit(1)
+			}
+
+			err = json.Unmarshal(bs, &m)
+			if err != nil {
+				fmt.Println("Manifest file doesn't appear to contain valid json: ", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	rootCmd.PersistentFlags().StringVarP(&manifestPath, "manifest", "m", "manifest.json", "Path were the manifest you want to apply is")
+	rootCmd.MarkPersistentFlagFilename("manifest")
+
+	applyManifestCmd.AddCommand(generateApplyManifestFsCmd(&m))
+
+	return applyManifestCmd
+}

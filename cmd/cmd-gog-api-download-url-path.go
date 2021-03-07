d9984ef706a9464e1fb02ca874f682720a084092
Eric Vallee <eric_vallee@webificservices.com>
2021-03-07 15:51:02 -0500

    Added specifiable directory for gog-api download command

--- cmd/cmd-gog-api-download-url-path.go
+++ cmd/cmd-gog-api-download-url-path.go
@@ -4,25 +4,34 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
 
 	"github.com/spf13/cobra"
 )
 
 func generateDownloadUrlPathCmd() *cobra.Command {
-	var path string
+	var relUrl string
+	var dir string
 
 	downloadUrlPathCmd := &cobra.Command{
 		Use:   "download-url-path",
 		Short: "Download a single file with the given path from GOG. Valid paths can be obtained from the manifest.",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			var err error
+			if dir == "" {
+				dir, err = os.Getwd()
+				processError(err)
+			}
+		},
 		Run: func(cmd *cobra.Command, args []string) {
-			body, _, file, err := sdkPtr.GetDownloadHandle(path)
+			body, _, file, err := sdkPtr.GetDownloadHandle(relUrl)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
 			defer body.Close()
 
-			out, fErr := os.Create(file)
+			out, fErr := os.Create(path.Join(dir, file))
 			if fErr != nil {
 				fmt.Println("Error creating file: ", fErr)
 				os.Exit(1)
@@ -37,8 +46,9 @@ func generateDownloadUrlPathCmd() *cobra.Command {
 		},
 	}
 
-	downloadUrlPathCmd.Flags().StringVarP(&path, "path", "p", "", "Url path to download")
+	downloadUrlPathCmd.Flags().StringVarP(&relUrl, "path", "p", "", "Url path to download")
 	downloadUrlPathCmd.MarkFlagRequired("path")
+	downloadUrlPathCmd.Flags().StringVarP(&dir, "directory", "r", "", "Directory to download the file in. will default to the current directory")
 
 	return downloadUrlPathCmd
 }

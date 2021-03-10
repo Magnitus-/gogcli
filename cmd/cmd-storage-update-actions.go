2f01472e5e49a5b668473a2d8b2ae9c4fae54528
Eric Vallee <eric_vallee@webificservices.com>
2021-03-09 22:52:15 -0500

    Completed storage update actions command

--- cmd/cmd-storage-update-actions.go
+++ cmd/cmd-storage-update-actions.go
@@ -1,13 +1,19 @@
 package cmd
 
 import (
+	"gogcli/manifest"
+	"gogcli/storage"
+
 	"github.com/spf13/cobra"
 )
 
 func generateStorageUpdateActionsCmd() *cobra.Command {
 	var m manifest.Manifest
 	var manifestPath string
-	
+	var path string
+	var storageType string
+	var allowEmptyCheckum bool
+
 	storageUpdateActionsCmd := &cobra.Command{
 		Use:   "update-actions",
 		Short: "Update the manifest and uncompleted actions in a storage given a newer manifest",
@@ -17,8 +23,17 @@ func generateStorageUpdateActionsCmd() *cobra.Command {
 			processError(err)
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-		}
+			gamesStorage, _ := getStorage(path, storageType, debugMode, "")
+			err := storage.UpdateActions(&m, gamesStorage, allowEmptyCheckum)
+			processError(err)
+		},
 	}
+
+	storageUpdateActionsCmd.Flags().StringVarP(&manifestPath, "manifest", "m", "manifest.json", "Path were the manifest you want to update the storage's actions with is")
+	storageUpdateActionsCmd.MarkFlagFilename("manifest")
+	storageUpdateActionsCmd.Flags().StringVarP(&path, "path", "p", "games", "Path to your games' storage (directory if it is of type fs, json configuration file if it is of type s3)")
+	storageUpdateActionsCmd.Flags().StringVarP(&storageType, "storage", "k", "fs", "The type of storage you are using. Can be 'fs' (for file system) or 's3' (for s3 store)")
+	storageUpdateActionsCmd.Flags().BoolVarP(&allowEmptyCheckum, "empty-checksum", "s", false, "If set to true, manifest files with empty checksums will count as already uploaded if everything else matches")
 
 	return storageUpdateActionsCmd
 }
---
+++ storage/update-actions.go
@@ -0,0 +1,52 @@
+package storage
+
+import (
+	"errors"
+	"gogcli/manifest"
+)
+
+func UpdateActions(nextMan *manifest.Manifest, s Storage, emptyChecksumOk bool) error {
+	hasSource, err := s.HasSource()
+	if err != nil {
+		return err
+	}
+	
+	if !hasSource {
+		return errors.New("No actions are pending in the storage. Aborting.")
+	}
+
+	storedMan, manErr := s.LoadManifest()
+	if manErr != nil {
+		return manErr
+	}
+
+	storedActions, actionsErr := s.LoadActions()
+	if actionsErr != nil {
+		return actionsErr
+	}
+
+	actionsUpdate := storedMan.Plan(nextMan, emptyChecksumOk)
+	updateErr := storedActions.Update(actionsUpdate)
+	if updateErr != nil {
+		return updateErr
+	}
+
+	if emptyChecksumOk {
+		err = nextMan.ImprintMissingChecksums(storedMan)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = s.StoreManifest(nextMan)
+	if err != nil {
+		return err
+	}
+
+	err = s.StoreActions(storedActions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

0d526c1b014a6974d0bfe93fe2f926f788fbb693
Eric Vallee <eric_vallee@webificservices.com>
2021-10-24 12:35:39 -0400

    Changed the storage repair command to use the storage's manifest by default

--- cmd/cmd-storage-repair.go
+++ cmd/cmd-storage-repair.go
@@ -14,14 +14,23 @@ func generateStorageRepairCmd() *cobra.Command {
 	var storageType string
 	var concurrency int
 	var verifyChecksum bool
+	var useFileManifest bool
 
 	storageRepairCmd := &cobra.Command{
 		Use:   "repair",
-		Short: "Upload the given manifest in the storage and generate the actions required to make the storage's game files an accurate reflection of the manifest",
+		Short: "Scan the storage's game files and generate the remedial actions required to ensure the storage's game files are an accurate reflection of the manifest",
 		PreRun: func(cmd *cobra.Command, args []string) {
 			var err error
-			m, err = loadManifestFromFile(manifestPath)
-			processError(err)
+
+			if useFileManifest {
+				m, err = loadManifestFromFile(manifestPath)
+				processError(err)
+				return
+			}
+
+			gamesStorage, _ := getStorage(path, storageType, logSource, "")
+			storageManifest, err := gamesStorage.LoadManifest()
+			m = (*storageManifest)
 		},
 		Run: func(cmd *cobra.Command, args []string) {
 			gamesStorage, _ := getStorage(path, storageType, logSource, "")
@@ -30,7 +39,8 @@ func generateStorageRepairCmd() *cobra.Command {
 		},
 	}
 
-	storageRepairCmd.Flags().StringVarP(&manifestPath, "manifest", "m", "manifest.json", "Manifest that you want to upload in your storage")
+	storageRepairCmd.Flags().BoolVarP(&useFileManifest, "file-manifest", "f", false, "If set to true, a specified manifest file will be used to repair the storage, otherwise the storage's manifest will be used")
+	storageRepairCmd.Flags().StringVarP(&manifestPath, "manifest", "m", "manifest.json", "Manifest file that you want to use to repair the storage")
 	storageRepairCmd.MarkFlagFilename("manifest")
 	storageRepairCmd.Flags().StringVarP(&path, "path", "p", "games", "Path to your games' storage (directory if it is of type fs, json configuration file if it is of type s3)")
 	storageRepairCmd.Flags().StringVarP(&storageType, "storage", "k", "fs", "The type of storage you are using. Can be 'fs' (for file system) or 's3' (for s3 store)")
